11f3be52c6690dc97e7bc99264e9f2ae3614b103
austinfrey <aafrey85@gmail.com>
2018-02-11 20:54:08 -0500

    2nd sha test

--- browser.js
+++ browser.js
@@ -8,12 +8,6 @@ function update() {
   yo.update(root, yo`
     <div>
       <h1>Hello</h1>
-      <h2>World</h2>
-      <h3>Goodbye</h3>
-      <h4>Cruel World</h4>
-      <h5>Last one</h5>
-      <h6>switched stacks</h6>
-      <p>now we getting crazy</p>
     </div>
   `)
 }
